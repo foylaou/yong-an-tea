3d8a10961f2ae55fca08352e402319336e04f806
Foy <s225002731@gmail.com>
2025-04-04 13:11:04 +0800

    finish components

---
+++ src/components/Layout/Layout.tsx
@@ -0,0 +1,11 @@
+import { ReactNode } from 'react';
+
+interface LayoutProps {
+    children: ReactNode;
+}
+
+function Layout({ children }: LayoutProps) {
+    return <main>{children}</main>;
+}
+
+export default Layout;

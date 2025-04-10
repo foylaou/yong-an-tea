65d0881cb376437374f41a1ad436af31e7dcdaf5
Foy <s225002731@gmail.com>
2025-04-10 12:14:05 +0800

    sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://yong-an-tea.vercel.app',
+  generateRobotsTxt: true,
+  sitemapSize: 5000,
+  changefreq: 'daily',
+  priority: 0.7,
+};

9ab6fc0bdcd8c15fa8fdcd6bd267977eb48c271e
Foy <s225002731@gmail.com>
2026-03-12 13:51:23 +0800

    feat:商品圖片支援多圖，優化客製化篩選

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import '@/styles/admin-globals.css';
-
+import { Analytics } from '@vercel/analytics/next';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 export const metadata = {
   title: '永安茶園 後台管理',
   description: '永安茶園 電商後台管理系統',
@@ -11,8 +12,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="zh-TW">
-      <body className="font-roboto antialiased">{children}</body>
-    </html>
+      <html lang="zh-TW">
+          <Analytics />
+          <SpeedInsights />
+          <body className="font-roboto antialiased">{children}</body>
+      </html>
   );
 }
